ec4d6ac66be10fcc34978e7d64f3bda7a6933b04
kevinav <kevinav@arquitecsoft.com>
2021-01-24 18:52:34 -0500

    Se configura puerto dinamico

--- ordena-backend/src/main.ts
+++ ordena-backend/src/main.ts
@@ -1,8 +1,15 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // getting the config service
+  const configService = app.get(ConfigService);
+
+  const PORT = configService.get<number>('config.app.port');
+
+  await app.listen(PORT);
 }
 bootstrap();
