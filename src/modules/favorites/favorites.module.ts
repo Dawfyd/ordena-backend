b9d0484a24e544f4ece811e1339d9976cc57efd2
KevinVelassco <kevinvelassco@gmail.com>
2021-02-24 21:13:21 -0500

    feat: in create a favorite only a pure product type can be favorite

--- src/modules/favorites/favorites.module.ts
+++ src/modules/favorites/favorites.module.ts
@@ -1,4 +1,4 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FavoritesService } from './favorites.service';
 import { FavoritesResolver } from './favorites.resolver';
@@ -6,12 +6,16 @@ import { Favorite } from './entities/favorite.entity';
 import { PersonsModule } from '../persons/persons.module';
 import { ProductsModule } from '../products/products.module';
 import { FavoritesLoaders } from './favorites.loaders';
+import { ParametersModule } from '../parameters/parameters.module';
+import { ProductTypesModule } from '../product-types/product-types.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Favorite]),
     PersonsModule,
-    forwardRef(() => ProductsModule)
+    ProductsModule,
+    ParametersModule,
+    ProductTypesModule
   ],
   providers: [FavoritesResolver, FavoritesLoaders, FavoritesService],
   exports: [FavoritesService]
