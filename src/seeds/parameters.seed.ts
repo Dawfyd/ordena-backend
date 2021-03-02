a6d15375bde59ff4849f618f1e7c95f9d802ee4f
Cristian David Ippolito Carrillo <cristiandavidippolito@hotmail.com>
2021-03-01 21:03:14 -0500

    feat: adding others parameters

--- src/seeds/parameters.seed.ts
+++ src/seeds/parameters.seed.ts
@@ -4,10 +4,42 @@ import { Parameter } from '../modules/parameters/entities/parameter.entity';
 export const ParameterFactory = {
   build: (connection: Connection): Parameter[] => {
     const items = [
+      // product types parameters
+      {
+        name: 'PRODUCT_TYPE_MENU',
+        value: '3PTM',
+        description: 'pending...'
+      },
+      {
+        name: 'PRODUCT_TYPE_ASSIGNED_CATEGORY',
+        value: '2PTAC',
+        description: 'pending...'
+      },
+      {
+        name: 'PRODUCT_TYPE_PURE',
+        value: '1PTP',
+        description: 'pending...'
+      },
+      {
+        name: 'PRODUCT_TYPE_ASSIGNED_PRODUCT',
+        value: '4PTAP',
+        description: 'pending...'
+      },
+      // person parameters
+      {
+        name: 'ADMIN_ROLE',
+        value: '01A',
+        description: 'pending...'
+      },
       {
         name: 'WAITER_ROLE',
         value: '03W',
         description: 'waiter role in ACL'
+      },
+      {
+        name: 'CUSTOMER_ROLE',
+        value: '02C',
+        description: 'pending...'
       }
     ];
 
